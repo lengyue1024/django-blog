f34c5a5e02aa8631def4ce7721bf51b0c7828e62
lengyue1024 <xzhxpx@qq.com>
2018-08-07 11:13:39 +0800

    my-django-blog

---
+++ myblog/blog/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from blog.models import BlogsPost
+
+# Create your views here.
+
+def blog_index(request):
+    blog_list = BlogsPost.objects.all()  # 获取所有数据
+    return render(request,'index.html', {'blog_list':blog_list})   # 返回index.html页面
